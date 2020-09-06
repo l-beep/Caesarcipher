7572daa6aec89a2f03631daf51e7b534d19bd47a
LOvine <oduor.lovine@gmail.com>
2020-09-06 20:39:38 +0300

    add files in the .gitignore file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,8 @@ public  class App {
     public static void main(String[] args) {
         Console myConsole = System.console();
         System.out.println("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG");
-        String stringLength = myConsole.readLine();
         String stringplainText = myConsole.readLine();
+        String stringplainText = myConsole.readLine();
+        int intShiftKey = Integer.parseInt(String)
     }
  }
