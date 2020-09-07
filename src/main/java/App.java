8256be75838dd4e38894b86bfd9a05cc2da27c84
LOvine <oduor.lovine@gmail.com>
2020-09-07 10:40:25 +0300

    add info to the readme

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,21 @@
 import java.io.Console;
 
-public  class App {
-    public static void main(String[] args) {
+public  class App<console> {
+    private static String
+            StringplainText;
+
+    public static <console> void main(String[] args) {
         Console myConsole = System.console();
         System.out.println("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG");
-        String stringplainText = myConsole.readLine();
-        String stringplainText = myConsole.readLine();
-        int intShiftKey = Integer.parseInt(String)
+        String InputString = myConsole.readLine();
+        int intShiftKey = Integer.parseInt(StringplainText);
+
+        System.out.println("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD");
+        String EncryptedStrings = myConsole.readLine();
+        intShiftKey = Integer.parseInt(StringplainText);
+
+        System.out.println("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG");
+        String DecryptedString = myConsole.readLine();
+        intShiftKey = Integer.parseInt(StringplainText);
     }
  }
